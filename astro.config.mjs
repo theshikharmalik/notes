51c0a1fc24f3aa8e8ca984ada1dd540118f5b42b
Shikhar Malik <46821489+theshikharmalik@users.noreply.github.com>
2026-07-07 01:44:53 -0400

    look overhauled and custom css added

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,20 +8,32 @@ export default defineConfig({
     base: '/notes/',
 	integrations: [
 		starlight({
-			title: 'My Notes',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'trashnotes',
+			favicon: '/favicon.png',
+			customCss: [
+        	'./src/styles/custom.css',
+      		],
+			tableOfContents: false,
+			components: {
+                Footer: './src/components/CustomFooter.astro',
+                SocialIcons: './src/components/CustomSocial.astro',
+            },
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'array',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-						{ label: 'Test Guide', slug: 'guides/test' },
+						{ label: 'two sum', slug: 'array/two_sum', badge: { text: 'easy', variant: 'success' } },
+						{ label: 'best time to buy and sell stock', slug: 'array/best_time_to_buy_and_sell_stock', badge: { text: 'easy', variant: 'success' } },						
+						{ label: 'three sum', slug: 'array/three_sum', badge: { text: 'med', variant: 'caution' } },
+						{ label: 'insert interval', slug: 'array/insert_interval', badge: { text: 'med', variant: 'caution' } },
+						{ label: 'merge intervals', slug: 'array/merge_intervals', badge: { text: 'med', variant: 'caution' } },
+						{ label: 'product of array except self', slug: 'array/product_of_array_except_self', badge: { text: 'med', variant: 'caution' } },
 					],
 				},
 				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'string',
+					items: [{ autogenerate: { directory: 'reference',  } }],
 				},
 			],
 		}),
